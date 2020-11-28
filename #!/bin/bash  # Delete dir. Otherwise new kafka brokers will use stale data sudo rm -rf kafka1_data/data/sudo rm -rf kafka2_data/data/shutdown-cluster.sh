d9732f9a858b54d4e244c3914d7cb349cd7da627
mmehta-10 <mmehta.ten@gmail.com>
2020-11-28 14:56:30 +0530

    Delete shutdown-cluster.sh

--- #!/bin/bash  # Delete dir. Otherwise new kafka brokers will use stale data sudo rm -rf kafka1_data/data/sudo rm -rf kafka2_data/data/shutdown-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Delete dir. Otherwise new kafka brokers will use stale data
-sudo rm -rf kafka1_data/data/
-sudo rm -rf kafka2_data/data/
-
-# Tear down cluster and volumes
-docker-compose down -v
